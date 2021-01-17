deb17218ad820371e3b606f1679be58abb2490a2
Mingyao Liu <MingyaoLiu@users.noreply.github.com>
2021-01-16 20:35:15 -0500

    Add files via upload

--- 2by3SwitchTesterKeyboard.ino
+++ 2by3SwitchTesterKeyboard.ino
@@ -11,11 +11,11 @@ uint8_t switch_down_count[6]; // switch continuous down state count.
 
 void setup() {
   
-  // Set pin 16 to high for LED
+  // Set pin 16 to high for LED.
   pinMode(16, OUTPUT);
   digitalWrite(16, HIGH);
   
-  // Set all button pin state
+  // Set all button pin state.
   for(uint8_t i = 0; i < 6; i++) {
     pinMode(switch_pin[i], INPUT_PULLUP);
   }
@@ -26,7 +26,7 @@ void loop() {
 
     for(uint8_t i = 0; i < 6; i++) {
       
-        // loop every pin current state and save to variable
+        // loop every pin current state and save to variable.
         switch_reading[i] = digitalRead(switch_pin[i]);
         press_these[i] = false;
         
@@ -36,31 +36,28 @@ void loop() {
         } else if (switch_reading[i] == 1 && switch_is_down[i]) { // Button is now up, and it was down before, reset down and down status count.
             switch_is_down[i] = false;
             switch_down_count[i] = 0;
-        } else if (switch_reading[i] == 0 && switch_is_down[i]) { // button is down, and it is holding down since before
+        } else if (switch_reading[i] == 0 && switch_is_down[i]) { // button is down, and it is holding down since before.
             if (switch_down_count[i] > 20) { // if its been n loop of down state, hold down state and output.
               press_these[i] = true;
             } else { // button hasn't hold long enough to continue register, up continuous state and not output yet.
               switch_down_count[i] += 1;
             }
-          
         }
-        
     }
 
-    // press each key that's in the press_these list
+    // press each key that's in the press_these list.
     for(uint8_t i = 0; i < 6; i++) {
         if (press_these[i]) {
             Keyboard.press(switch_key[i]);         
         }
     }
-    
     delay(30);
- 
+    
+    // release each key in the press_these list.
     for(uint8_t i = 0; i < 6; i++) {
         if (press_these[i]) {
             Keyboard.release(switch_key[i]);           
         }
     }
- 
     delay(10);
 }
